abfc55a9dc200e94c690a6d93fb2cde82c1bd836
gerald LELONG <lelongg@etud.rcisima.isima.fr>
2013-04-16 14:50:30 +0200

    ajout des fichiers de gestion de pile, du makefile, et d'un embryon de main

---
+++ stack.c
@@ -0,0 +1,83 @@
+#include "stack.h"
+#include <stdio.h>
+
+stack_t * createStack(size_t capacity)
+{
+    stack_t * stack=malloc(sizeof(stack_t));
+
+    if(stack)
+    {
+        stack->head=malloc(sizeof(stack_item_t)*capacity);
+
+        if(stack->head)
+        {
+            stack->size=0;
+            stack->capacity=capacity;
+            stack->topIndex=0;
+        }
+        else
+        {
+            free(stack);
+        }
+    }
+
+    return stack;
+}
+
+BOOL isEmpty(stack_t stack)
+{
+    return !stack.size;
+}
+
+BOOL isFull(stack_t stack)
+{
+    return stack.size>=stack.capacity;
+}
+
+stack_item_t * popStack(stack_t * stack)
+{
+    stack_item_t * item=0;
+
+    if(!isEmpty(stack))
+    {
+        (*item)=stack->head+stack->topIndex;
+
+        --(stack->size);
+        --(stack->topIndex);
+    }
+
+    return item;
+}
+
+int pushStack(stack_t * stack, stack_item_t item)
+{
+    int exit;
+
+    if(isFULL(stack))
+    {
+        exit=FAIL;
+    }
+    else
+    {
+        *(stack->head+stack->topIndex)=item;
+
+        ++(stack->size);
+        ++(stack->topIndex);
+
+        exit=SUCCESS;
+    }
+
+    return exit;
+}
+
+void printStack(stack_t stack)
+{
+    unsigned int i;
+
+    printf("%d items of %d in the stack\n", stack->size, stack->capacity);
+
+    for(i=0;i<stack->size;++i)
+    {
+        printf("Item %d: %d\n",i,*(stack->head+i));
+    }
+}
---
+++ stack.h
@@ -0,0 +1,32 @@
+/*
+ * @author Gerald lelong / Noel Martignoni
+ * @date 2013-04-16
+ *
+ * @file stack.h
+ * Définit les fonctions de base de manipulation
+ * des piles
+*/
+
+#ifndef STACK_H
+#define STACK_H
+
+typedef int stack_item_t;
+typedef struct
+{
+    stack_item_t * head;
+    size_t size;
+    size_t capacity;
+    unsigned int topIndex;
+} stack_t;
+
+stack_t * createStack(size_t capacity);
+
+BOOL isEmpty(stack_t stack);
+BOOL isFull(stack_t stack);
+
+stack_item_t * popStack(stack_t * stack);
+int pushStack(stack_t * stack, stack_item_t item);
+
+void printStack(stack_t stack);
+
+#endif
