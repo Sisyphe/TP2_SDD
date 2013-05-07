2fc8418142c51ad527e9dc107ebdd0e585ccdce3
noel MARTIGNONI <martigno@etud.rcisima.isima.fr>
2013-05-07 15:06:35 +0200

    ajout fichier test.c contient le parseur

---
+++ test.c
@@ -0,0 +1,61 @@
+dico a integrer a tree
+
+void buildTree()
+{
+	stack_t * stack = createStack(50);
+	
+	char * treeString = "(a(b(a(T)i(m(E)))))";
+	int index = 0;
+	
+	while (treeString[index] != '\0')
+	{		
+		
+		if (isOpenedParanthese(treeString[index]))
+		{
+			index++; /* Car suivant */
+			
+			/* addChild */
+			printf("empile %c \n", treeString[index]);
+			
+			/* empiler */
+			pushStack(stack, treeString[index]);
+		}
+		else if (isClosedParanthese(treeString[index]))
+		{
+			
+			/* depiler */
+			printf("depile %c\n", popStack(stack));
+			
+			if (! isClosedParanthese(treeString[index]))
+			{
+				index++;
+				
+				/* addSibling */
+			}
+		}
+		else if (isComma(treeString[index]))
+		{
+			index++;
+	
+			/* addSibling */
+		}
+
+		index++;
+		
+	}
+}
+
+int isOpenedParanthese(char car)
+{
+	return car == '(';
+}
+
+int isClosedParanthese(char car)
+{
+	return car == ')';
+}
+
+int isComma(char car)
+{
+	return car == ',';
+}
